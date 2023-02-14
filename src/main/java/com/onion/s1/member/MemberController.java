f1da860948317b722d91713eabce9daa438f75dc
qwdfd1 <qwdfd@naver.com>
2023-02-14 18:27:39 +0900

    0214session

--- src/main/java/com/onion/s1/member/MemberController.java
+++ src/main/java/com/onion/s1/member/MemberController.java
@@ -5,11 +5,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.context.request.SessionScope;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
